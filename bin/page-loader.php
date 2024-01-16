7f3285d7f6b45d2cd774e36376f6811b86888d5f
Petr Dobrokhotov <dobrokhotovp@gmail.com>
2024-01-16 23:43:43 +0400

    attempts 4

--- bin/page-loader.php
+++ bin/page-loader.php
@@ -6,24 +6,25 @@ use Hexlet\Code\Handler;
 use GuzzleHttp\Client;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-$url = $argv[1];
-$directory = '/home/petr/tmp';
-$client = new Client();
-$handler = new Handler();
-$handler->setArgs($argv);
-if ($handler->handleOptions()) {
-    //make new client, download page
-    $url = $handler->getUrl();
-    $directory = $handler->getDirectory();
-    downloadPage($url, $directory, $client);
-}
-
-function downloadPage(string $url, string $directory, Client $client)
-{
+if (! function_exists(__NAMESPACE__ . '\downloadPage')) {
+    $url = $argv[1];
+    $directory = '/home/petr/tmp';
+    $client = new Client();
     $handler = new Handler();
-    $args = ['page-loader', $url, '-o', $directory];
-    $handler->setArgs($args);
-    $url = $handler->getUrl();
-    $handler->downloadPage($url, $directory, $client);
+    $handler->setArgs($argv);
+    if ($handler->handleOptions()) {
+        //make new client, download page
+        $url = $handler->getUrl();
+        $directory = $handler->getDirectory();
+        downloadPage($url, $directory, $client);
+    }
+
+    function downloadPage(string $url, string $directory, Client $client)
+    {
+        $handler = new Handler();
+        $args = ['page-loader', $url, '-o', $directory];
+        $handler->setArgs($args);
+        $url = $handler->getUrl();
+        $handler->downloadPage($url, $directory, $client);
+    }
 }
